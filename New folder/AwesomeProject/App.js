793dde35b8d456923ac8bda66251928f0cf467ea
syed745 <syedb1997@gmail.com>
2022-07-26 23:44:21 +0500

    work complete

--- New folder/AwesomeProject/App.js
+++ New folder/AwesomeProject/App.js
@@ -7,17 +7,18 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  TextBase,
   TextInput,
   useColorScheme,
   View,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-
 import { styles } from "./style";
+
 
 function App() {
 
-  const [text, onChangeText] = React.useState("");
+  const [text, onChangeText] = React.useState();
   const [number, onChangeNumber] = React.useState(null);
 
   return (
@@ -71,102 +72,139 @@ function App() {
 
     // First Page End//
 
+    //Second Page //
 
-    <View style={{backgroundColor: '#83c5be',flex:1}}>
 
-      <View style={{backgroundColor:'white', width: "100%", height: 90 ,elevation: 100,}}>
-        <Text style={{ color: 'black', fontStyle: 'italic', fontSize: 40, margin: 12 , fontWeight:'bold'}}>
-          LOGIN FORM
+    // <View style={{ backgroundColor: '#83c5be', flex: 1 }}>
+
+    //   <View style={{ backgroundColor: 'white', width: "100%", height: 90, elevation: 100, }}>
+    //     <Text style={{ color: 'black', fontStyle: 'italic', fontSize: 40, margin: 12, fontWeight: 'bold' }}>
+    //       LOGIN FORM
+    //     </Text>
+    //   </View>
+
+    //   <View style={styles.container}>
+    //     <SafeAreaView>
+    //       <Text style={styles.circle}>
+    //         <Icon name='person' size={50} />
+    //       </Text>
+    //       <Text style={styles.email}>
+    //         EMAIL
+    //       </Text>
+    //       <TextInput
+    //         value={text}
+    //         placeholder="Enter Email"
+    //         style={styles.input}
+    //       />
+    //       <Text
+    //         style={styles.email}>
+    //         PASSWORD
+    //       </Text>
+    //       <TextInput
+    //         style={styles.input}
+    //         onChangeText={onChangeNumber}
+    //         value={number}
+    //         placeholder="Enter Password"
+    //         secureTextEntry={true}
+    //       />
+    //       <Pressable style={styles.email}>
+    //         <Text>
+    //           Already Have An Account
+    //           <Text style={{ color: 'blue' }}  > SIGNUP </Text>
+    //         </Text>
+    //       </Pressable>
+    //       <Text style={{ padding: 10, marginLeft: 80, }} >
+    //         {"\n"}
+    //         <View style={{ width: 100 }}>
+
+    //           <Button title="LOGIN" color="#2E8B57" />
+    //         </View>
+
+    //       </Text>
+
+    //     </SafeAreaView>
+    //   </View>
+    // </View>
+
+    //Second Page End //
+
+
+    // Third Page //
+
+
+    <View style={{ backgroundColor: '#caf0f8' }}>
+      <View style={{ backgroundColor: '#caf0f8', height: 200, }}>
+        <Text style={{ fontSize: 40, marginTop: 40, fontWeight: 'bold', textAlign: 'center' }}>
+          Login Account
         </Text>
+        <Text style={{ fontSize: 17, fontWeight: 'bold', textAlign: 'center' }}>
+          If You Have Account Please Login
+        </Text>
+
       </View>
+      <View style={{ backgroundColor: 'white', height: '100%', elevation: 30, borderTopRightRadius: 50, borderTopLeftRadius: 50, alignItems: 'center', }}>
+        <Text style={{
+          textAlign: 'center', marginTop: 20, backgroundColor: '#caf0f8', color: 'black', width: 100,
+          height: 90,
+          borderRadius: 60
+        }} >
 
-      <View style={styles.container}>
-      <SafeAreaView>
-           <Text style={styles.circle}>
-             <Icon name='person' size={50} />
-           </Text>
-           <Text style={styles.email}>
-             EMAIL
-           </Text>
-           <TextInput
-             value={text}
-             placeholder="Enter Email"
-             style={styles.input}
-           />
-           <Text
-             style={styles.email}>
-             PASSWORD
-           </Text>
-           <TextInput
-             style={styles.input}
-             onChangeText={onChangeNumber}
-             value={number}
-             placeholder="Enter Password"
-             secureTextEntry={true}
-           />
-           <Pressable  style={styles.email}>
-              <Text>
-                Already Have An Account 
-                <Text style={{color:'blue'}}  > SIGNUP </Text>
-              </Text>
-             </Pressable>
-           <Text style={{ padding: 10, marginLeft: 80, }} >
-             {"\n"}
-             <View style={{width:100}}>
-
-             <Button title="LOGIN" color="#2E8B57"  />
-             </View>
-             
-           </Text>
-          <Text>
-            <Icon name='instagram' size={20}/>
-            
+          <Icon name='person' size={80} style={{ color: 'caf0f8', padding: 3 }} />
+        </Text>
+        <SafeAreaView style={{ marginTop: 50 }} >
+          <Text style={styles.email}>
+            Email
           </Text>
+          <TextInput
+            style={{
+              width: 350,
+              height: 40,
+              margin: 12,
+              borderColor: '#011627',
+              borderWidth: 2,
+              padding: 5,
+              borderRadius: 10,
+            }}
+            placeholder='Enter Email'
+            autoCapitalize='email'
+          />
+          <Text style={styles.email}>
+            {"\n"}
+            Password
+          </Text>
+          <TextInput
+            style={{
+              width: 350,
+              height: 40,
+              margin: 12,
+              borderColor: '#011627',
+              borderWidth: 2,
+              padding: 5,
+              borderRadius: 10,
+            }}
+            onChangeText={onChangeNumber}
+            value={number}
+            placeholder="Enter Password"
+            keyboardType="password"
+            secureTextEntry={true}
+          />
+          <Pressable style={styles.email}>
+            <Text style={{ marginTop: 8 }}>
+              Already Have An Account
+              <Text style={{ color: 'blue' }}  > SIGNUP </Text>
+              <Text>
+                {"\n"}
+              </Text>
+            </Text>
+          </Pressable>
+          <Button title='LOGIN'
+            color='green' />
         </SafeAreaView>
-      </View>
 
+      </View>
     </View>
+    // Third Page end //
   )
 }
 
 export default App
-
-// const styles = StyleSheet.create({
-//   container: {
-//     height: 400,
-//     width: 310,
-//     backgroundColor: "white",
-//     alignSelf: 'center',
-//     marginTop: 90,
-//     flexDirection: 'row',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     elevation: 10,
-//     borderRadius: 10
-//   },
-//   titleText: {
-//     fontSize: 30,
-//     fontWeight: "bold",
-//     alignSelf: 'center',
-//     marginTop: 90,
-//     color: '#a8dadc',
-//   }, input: {
-//     width: 250,
-//     height: 40,
-//     margin: 12,
-//     borderColor: '#011627',
-//     borderWidth: 2,
-//     padding: 5,
-//     borderRadius: 10,
-//   },
-//   circle: {
-//     textAlign: 'center', fontWeight: "bold", marginBottom: 30, marginLeft: 110, backgroundColor: '#011627', color: 'white', width: 60,
-//     height: 60,
-//     borderRadius: 44 / 2
-//   },
-//   email: {
-//     marginLeft: 15, fontSize: 15,
-//     fontWeight: "bold",
-//   }
-
-// });
